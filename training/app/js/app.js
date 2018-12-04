44119d04f657dedf2170205251fa5d86505303e8
OliBravo <33745054+OliBravo@users.noreply.github.com>
2018-12-04 23:14:52 +0100

    videos allowed (sceDelegateProvider)

--- training/app/js/app.js
+++ training/app/js/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
-angular.module('app', ['ngRoute', '7minWorkout'])
-    .config(function ($routeProvider) {
+angular.module('app', ['ngRoute', '7minWorkout']).config(function ($routeProvider, $sceDelegateProvider) {
         $routeProvider.when('/start', {
             templateUrl: 'partials/start.html'
         });
@@ -15,6 +14,14 @@ angular.module('app', ['ngRoute', '7minWorkout'])
         $routeProvider.otherwise({
             redirectTo: '/start'
         });
+
+        $sceDelegateProvider.resourceUrlWhitelist([
+            // zezwalamy na zasoby z tego samego źródła
+            'self',
+            // A teraz zezwalamy na wczytywanie zasobów z innych domen.  
+            // Warto zwrócić uwagę na różnice pomiędzy symbolami * oraz **.
+            'http://*.youtube.com/**']);
+      
     });
 
 angular.module('7minWorkout', []);
